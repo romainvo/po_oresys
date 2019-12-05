da61202603a20c51883afd135bee768d86730f36
Romain Vo <romain.vo@hotmail.fr>
2019-12-05 16:04:32 +0100

    croisement effectif rpls/airbnb

--- croisement_v2.py
+++ croisement_v2.py
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 16:42:59 2019
+
+@author: romain
+"""
+
 import pandas as pd
 import geopandas as gpd
+import numpy as np
+import multiprocessing
+from functools import partial
 
 def converter_cp(string):
     try:
@@ -7,59 +17,87 @@ def converter_cp(string):
     except:
         return 0
 
-# importation des données, pour l'instant sans nettoyage préalable : on se contente d'ignorer les lignes ayant le mauvais format.
-reader = pd.read_csv("geo-rpls2017.csv", sep=',',error_bad_lines=False, header='infer',            
-                     usecols=['codepostal','numvoie','typvoie','nomvoie'
-                              ,'numappt','longitude','latitude','geo_type'
-                              ,'geo_adresse'],
-                     converters={'codepostal':converter_cp})
+def haversine(lon1, lat1, lon2=1.0, lat2=1.0):
+
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    r = 6371008 # Radius of earth in meters
+    d = 2 * r * np.arcsin(np.sqrt(a)) 
+
+    return d
+
+def func(chunk_bnb, data_rpls=None):
+    for idx, row in enumerate(chunk_bnb.itertuples()):
+        print(row.id_bnb)
+        distances = haversine(data_rpls.longitude, data_rpls.latitude
+                              , lon2=row.longitude, lat2=row.latitude)
+        chunk_bnb.at[idx, 'id_rpls'] = distances.idxmin()
+        chunk_bnb.at[idx, 'dist'] = distances.min()
+
+#data_airbnb = pd.read_csv("airbnb.csv", sep=',', header='infer',
+#                          usecols=['street','neighbourhood', 'neighbourhood_cleansed'
+#                                   ,'city','state','zipcode'
+#                                   ,'country_code', 'country'
+#                                   ,'latitude','longitude', 'is_location_exact'],
+#                          dtype={'longitude':'float', 'latitude':'float'})
+    
+data_rpls = pd.read_csv("paris_rpls_2017.csv", sep=',',error_bad_lines=False, 
+                        header='infer', 
+                        usecols=['codepostal','longitude','latitude','id_geo-rpls'],
+                        converters={'codepostal':converter_cp},
+                        dtype={'longitude':'float', 'latitude':'float'})
 
 data_airbnb = pd.read_csv("airbnb.csv", sep=',', header='infer',
-                          usecols=['street','neighbourhood', 'neighbourhood_cleansed'
-                                   ,'city','state','zipcode','market'
-                                   ,'smart_location', 'country_code', 'country'
-                                   ,'latitude','longitude', 'is_location_exact'])
+                          usecols=['latitude','longitude'],
+                          dtype={'longitude':'float', 'latitude':'float'})
 
-data_rpls = reader.loc[(reader['codepostal'] >= 75000) & (reader['codepostal'] < 76000)]
+data_airbnb.loc[:, 'id_bnb'] = data_airbnb.index.astype(int)
 
-#data_rpls = reader.get_chunk(100000)
+#-----------------------------------------------------------------------------#
+#-------------------------- WITH DataFrame.apply -----------------------------#
+#-----------------------------------------------------------------------------#
 
-data_rpls.loc[:, 'id_rpls'] = data_rpls.index
-data_airbnb.loc[:, 'id_bnb'] = data_airbnb.index
 
-data_rpls.reset_index(drop=True, inplace=True)
+def func_apply(row, data_rpls=None):
+    print(row.id_bnb)
+    distances = haversine(data_rpls.longitude, data_rpls.latitude
+                          , lon2=row.longitude, lat2=row.latitude)
+    
+    return [distances.idxmin(), distances.min()]
 
-merge = pd.concat([data_rpls, data_airbnb])
+results = data_airbnb.apply(partial(func_apply,data_rpls=data_rpls)
+, axis=1, result_type='expand')
 
-merge.loc[:,'new_id'] = merge.groupby([merge.latitude.round(4), merge.longitude.round(4)]).ngroup()
+results.columns = ['id_rpls', 'distance']
+results.index.rename('id_bnb', inplace=True)
+#results.to_csv('results.csv', header=True)
 
-merge.sort_values(by='new_id', inplace=True)
+#-----------------------------------------------------------------------------#
+#------------------------- WITH MULTIPROCESSING ------------------------------#
+#-----------------------------------------------------------------------------#
 
-#data_rpls = gpd.GeoDataFrame(
-#    data_rpls, geometry=gpd.points_from_xy(data_rpls.longitude, data_rpls.latitude))
+#data_airbnb.loc[:,'id_rpls'] = -1
+#data_airbnb.loc[:, 'dist'] = -1
 #
-#dataAirbnb = gpd.GeoDataFrame(
-#    data_airbnb, geometry=gpd.points_from_xy(data_airbnb.longitude, data_airbnb.latitude))
-
-"""
-# On s'interesse uniquement aux données de latitude et longitude des différentes annonces airbnb.
-dfLocalisationAirbnb = dataAirbnb.loc[:,['id_bnb','latitude','longitude']]
-dfLocalisationRPLS = dataRPLS.loc[:,['id_rpls','latitude','longitude']]
-
-dfLocalisationAirbnb.drop_duplicates(subset=['latitude','longitude'], inplace=True)
-
-dfLocalisationAirbnb.fillna(0.0, inplace=True)
-dfLocalisationRPLS.fillna(0.0, inplace=True)
-
-dfLocalisationAirbnb.sort_values(by='latitude', inplace=True)
-dfLocalisationRPLS.sort_values(by='latitude', inplace=True)
-MergedLat = pd.merge_asof(dfLocalisationAirbnb,dfLocalisationRPLS
-                        ,on='latitude',direction='nearest')
-
-dfLocalisationAirbnb.sort_values(by='longitude', inplace=True)
-dfLocalisationRPLS.sort_values(by='longitude', inplace=True)
-MergedLong = pd.merge_asof(dfLocalisationAirbnb,dfLocalisationRPLS
-                        ,on='longitude',direction='nearest')
-
-MergedTotal = MergedLat.merge(MergedLong,how='inner',on=['id_bnb','id_rpls'])
-"""
+#num_processes = multiprocessing.cpu_count()
+#
+## calculate the chunk size as an integer
+#chunk_size = int(data_airbnb.shape[0]/num_processes)
+#
+#chunks = [data_airbnb.iloc[data_airbnb.index[i:i + chunk_size]] \
+#          for i in range(0, data_airbnb.shape[0], chunk_size)]
+#
+## create our pool with `num_processes` processes
+#pool = multiprocessing.Pool(processes=num_processes)
+#
+## apply our function to each chunk in the list
+#result = pool.map(partial(func, data_rpls=data_rpls), chunks)
+#
+#for i in range(len(result)):
+#   # since result[i] is just a dataframe
+#   # we can reassign the original dataframe based on the index of each chunk
+#   data_airbnb.iloc[result[i].index] = result[i]
