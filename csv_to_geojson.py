ee0484dfc8bf14c0680740b82aeac299213268f6
Romain Vo <romain.vo@hotmail.fr>
2019-12-06 14:06:01 +0100

    convert results.csv to .geojson

---
+++ csv_to_geojson.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 13:44:10 2019
+
+@author: romain
+"""
+
+import pandas as pd
+import geopandas as gpd
+
+df = pd.read_csv('paris_rpls_2017.csv', sep=',', header='infer',
+                 usecols=['codepostal','numvoie','typvoie','nomvoie','longitude',
+                          'latitude'])
+
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
+gdf.drop(columns=['longitude','latitude'], inplace=True)
+gdf.to_file("rpls.geojson", driver='GeoJSON')
