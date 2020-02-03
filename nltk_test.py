108a71fe07ecf7daf245f833cc405a38230d8cc7
Romain Vo <romain.vo@hotmail.fr>
2020-02-03 16:06:42 +0100

    test nltk sur data_airbnb

--- nltk_test.py
+++ nltk_test.py
@@ -1,36 +1,39 @@
 import nltk 
 nltk.data.path.append("D:\\Users\\romai\\nltk_data")
 import pandas as pd
+import re, pprint
 import numpy as numpy
+                
+from nltk.book import * 
 
 data_airbnb = pd.read_csv("airbnb.csv", sep=',', header='infer',
                           dtype={'longitude':'float', 'latitude':'float'})
+
+print((data_airbnb.is_location_exact == 't').sum())
           
 #Colonnes à analyser dans les données airbnb
 # summary / space / description / neighorhood_overview / street / neighbourhood
 # neighbourhoud_cleansed / neighbourhood_group_cleansed / city / 
-                
-from nltk.book import * 
+# room_type / bathrooms / bedrooms / beds / square_feet / property_type
+# longitude / latitude
 
-#Words in context
-text1.concordance("monstruous")
-text2.concordance("affection")
+# nltk object 
+desc = nltk.Text(nltk.word_tokenize(data_airbnb.iloc[638].description, language='english'))
 
-#What other words appear in a similar range of contexts
-text2.similar("affection")
+#part_of_speech tagging
+# nltk.help.upenn_tagset('VBP') : meaning of tag 
+pos_desc = nltk.pos_tag(desc)
 
-#Examine the contextes that are shared by two or more words
-text2.common_contexts(["affection","very"])
+#Now we’ll implement noun phrase chunking to identify named entities using a
+# regular expression consisting of rules that indicate how sentences should be chunked.
+#Our chunk pattern consists of one rule, that a noun phrase, NP, should be 
+#formed whenever the chunker finds an optional determiner, DT, followed by any 
+#number of adjectives, JJ, and then a noun, NN.
+pattern = 'SFHAB: {<CD>+<NNS>+<JJ>+}'
 
-#positional information can be displayed using 
-#a dispersion plot. Each stripe represents an 
-#instance of a word and each row represents the 
-#entire text
-text4.dispersion_plot(
-        ["citizens", "democracy", "freedom", "duties", "America"])
+pattern = 'SFHAB: {"<CD>+\s+(?:mètres)\s+"}'
 
-def lexical_diversity(text):
-    return len(set(text)) / len(text)
+cp = nltk.RegexpParser(pattern)
 
-def percentage(count, total):
-    return 100 * count / total
+result = cp.parse(pos_desc) 
+print(result)
