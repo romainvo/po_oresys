ce84bb62bb90e28c4a4ebe85a598a08d822dcc96
Romain Vo <romain.vo@hotmail.fr>
2020-02-03 17:14:25 +0100

    ajout de règles de détection pour surface habitable

--- nltk_test.py
+++ nltk_test.py
@@ -2,14 +2,18 @@ import nltk
 nltk.data.path.append("D:\\Users\\romai\\nltk_data")
 import pandas as pd
 import re, pprint
-import numpy as numpy
+import numpy as np
                 
 from nltk.book import * 
+from nltk.text import TokenSearcher
 
 data_airbnb = pd.read_csv("airbnb.csv", sep=',', header='infer',
                           dtype={'longitude':'float', 'latitude':'float'})
 
-print((data_airbnb.is_location_exact == 't').sum())
+summary_airbnb = data_airbnb.loc[:, 'description']
+description_airbnb = data_airbnb.loc[:, 'description']
+
+print((data_airbnb.is_location_exact == 't').sum(), 'b')
           
 #Colonnes à analyser dans les données airbnb
 # summary / space / description / neighorhood_overview / street / neighbourhood
@@ -18,22 +22,9 @@ print((data_airbnb.is_location_exact == 't').sum())
 # longitude / latitude
 
 # nltk object 
-desc = nltk.Text(nltk.word_tokenize(data_airbnb.iloc[638].description, language='english'))
+description_tokens = nltk.word_tokenize(description_airbnb.iloc[653], language='english')
+description_nltk = nltk.Text(description_tokens)
 
-#part_of_speech tagging
-# nltk.help.upenn_tagset('VBP') : meaning of tag 
-pos_desc = nltk.pos_tag(desc)
-
-#Now we’ll implement noun phrase chunking to identify named entities using a
-# regular expression consisting of rules that indicate how sentences should be chunked.
-#Our chunk pattern consists of one rule, that a noun phrase, NP, should be 
-#formed whenever the chunker finds an optional determiner, DT, followed by any 
-#number of adjectives, JJ, and then a noun, NN.
-pattern = 'SFHAB: {<CD>+<NNS>+<JJ>+}'
-
-pattern = 'SFHAB: {"<CD>+\s+(?:mètres)\s+"}'
-
-cp = nltk.RegexpParser(pattern)
-
-result = cp.parse(pos_desc) 
-print(result)
+pattern_surfhab = r"<.*><m.tre.><carre.*>|<..m2>"
+    
+TokenSearcher(description_nltk).findall(pattern_surfhab)
