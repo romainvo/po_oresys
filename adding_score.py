fa47dc7e3dee976f5d65e1efab1c8d5afae3f953
AjayMouttou <ajaymouttoupro@gmail.com>
2020-02-09 21:53:09 +0100

    adding score columns to results

---
+++ adding_score.py
@@ -0,0 +1,16 @@
+import sys,os,path,glob
+import pandas as pd
+
+files = 'results_rd150_nb100.csv'
+
+df = pd.read_csv(files, nrows = 500)
+#df = df.convert_objects(convert_numeric=True)
+count = 3
+while count<=len(df.columns) :
+    df.insert(count,'score'+str((int(count/3)-1)),5,allow_duplicates = True)
+    print(count)
+    count=count+3
+
+df.to_csv(files,index=False)
+
+ 
