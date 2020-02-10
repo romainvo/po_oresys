23e482f43f97825b6937cd32f14a676d6c3aec3f
qseite <quentin.seite@hotmail.fr>
2020-02-10 16:07:25 +0100

    Modification methode de scoring pour etre plus adapté à la sortie nlp

--- scoring.py
+++ scoring.py
@@ -1,13 +1,11 @@
 import numpy as np
-""" surface et nombre de piece réel issu du repertoire RPLS, Théorique issu de l'extraction d'information"""
+""" surface et nombre de piece réel issu du repertoire RPLS, Théorique issu de l'extraction d'information
 
 def CalculScore(surfaceTheo, surfaceReel, nbpieceTheo, nbpieceReel):
     score = ((np.minimum(surfaceTheo/surfaceReel,surfaceReel/surfaceTheo)*np.minimum(surfaceReel/surfaceTheo,surfaceTheo/surfaceReel))/1.25)*100
-    """ on impose un score minimum de 0.25 du fait de la proximité, les 0.75 restants etant calculés via la formule précédente"""
+    on impose un score minimum de 0.25 du fait de la proximité, les 0.75 restants etant calculés via la formule précédente
     score = 25+score
-
-""" DRAFT
-def ScoreGlobal():
-    simple compte des scores > à un certain seuil?
-  
 """
+def calculScore(scoreSurface,scoreNbPiece):
+    scoreTotal=25+(scoreSurface*scoreNbPiece)*100
+
