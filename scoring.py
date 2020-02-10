83c8783aace9b27c0db529fb7e950ffba7ce735b
qseite <quentin.seite@hotmail.fr>
2020-02-10 10:54:11 +0100

    Calcul en utilisant np

--- scoring.py
+++ scoring.py
@@ -1,7 +1,8 @@
+import numpy as np
 """ surface et nombre de piece réel issu du repertoire RPLS, Théorique issu de l'extraction d'information"""
 
 def CalculScore(surfaceTheo, surfaceReel, nbpieceTheo, nbpieceReel):
-    score = (min(surfaceTheo/surfaceReel,surfaceReel/surfaceTheo)*min(surfaceReel/surfaceTheo,surfaceTheo/surfaceReel))/1.25
+    score = (np.minimum(surfaceTheo/surfaceReel,surfaceReel/surfaceTheo)*np.minimum(surfaceReel/surfaceTheo,surfaceTheo/surfaceReel))/1.25
     """ on impose un score minimum de 0.25 du fait de la proximité, les 0.75 restants etant calculés via la formule précédente"""
     score = 0.25+score
 
